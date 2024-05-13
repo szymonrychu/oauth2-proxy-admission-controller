53e0c3e55ef2269746f8696b0b47eaaafff826d9
Szymon Richert <szymon.rychu@gmail.com>
2024-05-13 13:58:40 +0200

    feat: Add tests for utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+from oauth2_proxy_admission_controller.utils import (
+    b64dec,
+    b64dec_json,
+    b64enc,
+    b64enc_json,
+)
+
+
+def test_b64enc():
+    assert b64enc("test_data") == "dGVzdF9kYXRh"
+
+
+def test_b64dec():
+    assert b64dec("dGVzdF9kYXRh") == "test_data"
+
+
+def test_b64enc_json():
+    assert b64enc_json({"test": "test"}) == "eyJ0ZXN0IjogInRlc3QifQ=="
+
+
+def test_b64dec_json():
+    assert b64dec_json("eyJ0ZXN0IjoidGVzdCJ9") == {"test": "test"}
